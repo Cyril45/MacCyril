966a5d951be68a54704ca8333ec7a7d4b28ceaf7
SIMONIN Cyril <simonin.cyril91@gmail.com>
2019-01-19 09:24:46 +0100

    Pep8 validé

--- functions.py
+++ functions.py
@@ -1,11 +1,12 @@
 class Var_init:
+
     def __init__(self):
-        self.full_map = [] #map with all the items in place.
-        self.items = [] # List of position of items
-        self.end = [] #end position
-        self.start = [] #start position
-        self.road = [] # list of position of roads
-        self.wall = [] # list of position of wall
+        self.full_map = []  # map with all the items in place.
+        self.items = []  # List of position of items
+        self.end = []  # end position
+        self.start = []  # start position
+        self.road = []  # list of position of roads
+        self.wall = []  # list of position of wall
         self.position = ()
         self.message = ("\
             z : for an upward movement\n \
@@ -13,15 +14,14 @@ class Var_init:
             q : for a movement to the left\n \
             d : for movement to the right\n\n \
             Exit to leave")
-
         self.end_player = False
         self.x = int
         self.y = int
 
-class Initialize_map:
-    def __init__(self,player):
-        self.player = player
 
+class Initialize_map:
+    def __init__(self, player):
+        self.player = player
         self.load_data_map()
         self.create_blank_map()
 
@@ -33,68 +33,68 @@ class Initialize_map:
                     self.player.y = y
                     if case == " ":
                         self.player.road.append((x, y))
-                    elif case == "D": 
+                    elif case == "D":
                         self.player.start.append((x, y))
                         self.player.position = (x, y)
-                    elif case == "A": 
+                    elif case == "A":
                         self.player.end.append((x, y))
-                    elif case =="O":
+                    elif case == "O":
                         self.player.items.append((x, y))
                     else:
                         self.player.wall.append((x, y))
 
     def create_blank_map(self):
-        self.player.full_map= [[""]]*(self.player.x+1)
-        numberliste = 0
-        while numberliste < (self.player.x+1):
-            self.player.full_map[numberliste] = [""]*(self.player.y+1)
-            numberliste +=1
+        self.player.full_map = [[""]]*(self.player.x+1)
+        numberlist = 0
+        while numberlist < (self.player.x+1):
+            self.player.full_map[numberlist] = [""]*(self.player.y+1)
+            numberlist += 1
 
 
 class Move_map:
-    def __init__(self,player):
+    def __init__(self, player):
         self.player = player
 
     def movement(self, direction):
         if direction == "s":
             x, y = self.player.position
-            x +=1
-            if self.check_position(x,y) == False:
-                x -=1
+            x += 1
+            if self.check_position(x, y) is False:
+                x -= 1
                 Print_map(self.player).printt(self.player.position)
             else:
-                self.player.position = (x,y)
+                self.player.position = (x, y)
                 Print_map(self.player).printt(self.player.position)
 
         elif direction == "z":
             x, y = self.player.position
-            x -=1
-            if self.check_position(x,y) == False:
-                x +=1
+            x -= 1
+            if self.check_position(x, y) is False:
+                x += 1
                 Print_map(self.player).printt(self.player.position)
             else:
-                self.player.position = (x,y)
+                self.player.position = (x, y)
                 Print_map(self.player).printt(self.player.position)
 
         elif direction == "q":
             x, y = self.player.position
-            y -=1
-            if self.check_position(x,y) == False:
-                y +=1
+            y -= 1
+            if self.check_position(x, y) is False:
+                y += 1
                 Print_map(self.player).printt(self.player.position)
             else:
-                self.player.position = (x,y)
-                Print_map(self.player).printt(self.player.position)
-        elif direction == "d":
-            x, y = self.player.position
-            y +=1
-            if self.check_position(x,y) == False:
-                y -=1
-                Print_map(self.player).printt(self.player.position)
-            else:
-                self.player.position = (x,y)
+                self.player.position = (x, y)
                 Print_map(self.player).printt(self.player.position)
 
+        elif direction == "d":
+            x, y = self.player.position
+            y += 1
+            if self.check_position(x, y) is False:
+                y -= 1
+                Print_map(self.player).printt(self.player.position)
+            else:
+                self.player.position = (x, y)
+                Print_map(self.player).printt(self.player.position)
 
         elif direction == "exit":
             self.player.end_player = True
@@ -104,25 +104,29 @@ class Move_map:
             print(self.player.message)
             Print_map(self.player).printt(self.player.position)
 
-    def check_position(self,position_x, position_y):
-        if position_x < 0 or position_x > self.player.x or position_y < 0 or position_y > self.player.y:
+    def check_position(self, position_x, position_y):
+        if position_x < 0 or position_y < 0 or \
+         position_x > self.player.x or position_y > self.player.y:
             print("You can’t get out of the maze")
             return False
-        
+
         elif ((position_x, position_y) in self.player.wall):
             print("You can’t cross the walls")
             return False
+
         elif ((position_x, position_y) in self.player.items):
             index = self.player.items.index((position_x, position_y))
             r = self.player.items.pop(index)
             self.player.road.append(r)
             print("You got back an object")
             return True
+
         else:
             return True
 
+
 class Print_map:
-    def __init__(self,player):
+    def __init__(self, player):
         self.player = player
 
     def printt(self, position):
@@ -130,40 +134,37 @@ class Print_map:
         for a in self.player.full_map:
             print(a)
 
-    def load_data_user(self,position):
+    def load_data_user(self, position):
         for z in self.player.items:
-            x,y =z
+            x, y = z
             if position == z:
                 self.player.full_map[x][y] = "M"
             else:
-                self.player.full_map[x][y]="O"
+                self.player.full_map[x][y] = "O"
 
         for z in self.player.end:
-            x,y =z
+            x, y = z
             if position == z:
                 self.player.full_map[x][y] = "M"
-
-                if not self.player.items :
+                if not self.player.items:
                     self.player.end_player = True
                 else:
                     print("you have not recovered all the objects")
             else:
                 self.player.full_map[x][y] = "A"
-                
-        for z in self.player.start:
-            x,y =z  
-            if position == z:
-                self.player.full_map[x][y] = "M"
-            else:
-                self.player.full_map[x][y] = "D"
 
-        for z in self.player.road:
-            x,y =z
+        for z in self.player.start:
+            x, y = z
             if position == z:
                 self.player.full_map[x][y] = "M"
             else:
                 self.player.full_map[x][y] = " "
-
+        for z in self.player.road:
+            x, y = z
+            if position == z:
+                self.player.full_map[x][y] = "M"
+            else:
+                self.player.full_map[x][y] = " "
         for z in self.player.wall:
-            x,y =z
+            x, y = z
             self.player.full_map[x][y] = "#"
--- labyrinthe.py
+++ labyrinthe.py
@@ -1,17 +1,19 @@
 import functions
+
+
 def main():
+
     player = functions.Var_init()
     functions.Initialize_map(player)
     print(player.message)
     functions.Print_map(player).printt(player.position)
-
-    while player.end_player == False:
+    while player.end_player is False:
         direction = input("enter: ")
         functions.Move_map(player).movement(direction.lower())
-
-    if player.end_player == True and direction != "exit":
+    if player.end_player is False and direction != "exit":
         print("!!!!!!! Bravo you are victorious !!!!!!!")
     else:
         print("You left before the end, Damage !!!")
+
 
 main()
